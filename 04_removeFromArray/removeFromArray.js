c9a9f6115cc204ea134571d5ae2d942c7294b6af
Brandon <b63822751@gmail.com>
2025-01-22 19:41:49 -0400

    fourd exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...argument) {
+
+    const newArray = [];
+
+    array.forEach(element => {
+       if(!argument.includes(element)){
+        newArray.push(element);
+       } 
+    });
+    return newArray;
+
+   
+
 };
 
 // Do not edit below this line
