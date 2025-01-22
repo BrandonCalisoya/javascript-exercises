c5a05e557e9ccbad24985db75eb99e0b2d3eebcd
Brandon <b63822751@gmail.com>
2025-01-22 19:40:36 -0400

    second completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatWord="";
+    if(num>=0){
+        
+    for (let i = 0; i < num; i++) {
+        repeatWord = repeatWord + string;
+    }
+    return repeatWord;
+    }{
+        return repetaWord = "ERROR";
+    }
 };
 
 // Do not edit below this line
