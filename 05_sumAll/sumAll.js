2b667456d88569e248d065b29b62b44ed89d8f60
Brandon <b63822751@gmail.com>
2025-01-22 19:42:53 -0400

    fifth exersice completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1,num2) {
+    let sum = 0;
+    
+    if(num1<0 ||num2<0) return 'ERROR';
+    if(!Number.isInteger(num1)||!Number.isInteger(num2))return 'ERROR';
+
+    if(num1>num2){
+        let aux = num1;
+        num1 = num2;
+        num2= aux;
+    }
+    for (let i = 0; i < num2; i++) {
+        sum += num1++;
+        
+    }
+
+    return sum;
 
 };
 
