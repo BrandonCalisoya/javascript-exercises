8d3f542a0a8216b935a3bd4d260f37cefa391bbb
Brandon <b63822751@gmail.com>
2025-01-28 11:33:44 -0400

    calculator done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
+const add = function(...args) {
+
+  return args.reduce((first, current)=> first +current);
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
+const subtract = function(...args) {
+  return args.reduce((first, current)=> first -current);
 
 };
 
-const power = function() {
-	
+const sum = function(args=[]) {
+  if(args== 0){
+    return 0
+  }
+  if(args.length == 1) return args[0]
+  let sum=0
+  sum =   args.reduce((first, current)=> first+ current);
+
+  
+  return sum;
+
 };
 
-const factorial = function() {
-	
+const multiply = function(args=[]) {
+  let result =args.reduce((first, current)=> first *current);;
+  
+  return result;
+
+};
+
+const power = function(num, exp) {
+  
+  let numActual = num;
+  for(let i =1; i< exp; i++){
+    num *= numActual 
+  }
+	return num;
+};
+
+const factorial = function(num) {
+  if(num == 0 || num == 1)return 1
+    let count = num;
+    let aux = num;
+  for (let i = 0; i < count-1; i++) {
+    num = num * (--aux);
+  }
+	return num;
 };
 
 // Do not edit below this line
