e5280c396e2ed52addd3999a4a81334f233423d3
Brandon <b63822751@gmail.com>
2025-01-28 12:34:10 -0400

    palindromes done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const cleanSentence = word.toLowerCase().replace(/[^a/zA-Z0-9]/g, '');
+    let start = 0;
+    let end = cleanSentence.length -1;
+    while (start < end ){
+        if(cleanSentence[start] !== cleanSentence[end]){
+            return false;
+        }
+        start++;
+        end--;
+    }
+    return true
 };
 
 // Do not edit below this line
